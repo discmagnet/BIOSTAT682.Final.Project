08c4bb95f633d4dd3aae49f850978581b4131103
YuanJinn <35829221+YuanJinn@users.noreply.github.com>
2018-12-09 22:20:34 -0500

    Update exponential.R

--- exponential.R
+++ exponential.R
@@ -9,134 +9,75 @@ pbc$female <- 1*(pbc$sex == 1)
 pbc$stage4 <- 1*(pbc$stage == 4)
 pbc$edema1 <- 1*((pbc$edema == 1)|(pbc$edema == 0.5))
 
-# covariates
-X <- pbc[,c("drug","sex","ascites","hepatom","spiders","edema1","age","bili","chol","albumin","copper","alk","sgot","trig","platelet","prothrombin","stage4")]
+x <- pbc[,c("drug","sex","ascites","hepatom","spiders","edema1","age","bili","chol","albumin","copper","alk","sgot","trig","platelet","prothrombin","stage4")]
 
-# event time
-Y = pbc$time
-is.na(Y) <- pbc$status ==0
+t<-pbc$time
+is.na(t)<-pbc$status==0
 
-# censoring time
-is_censored = 1 - pbc$status
-Y_cen <- pbc$time + pbc$status
-# censoring time of dead people must be greated than their observed event time
+is.censored<-1-pbc$status
+t.cen<-pbc$time+10*pbc$status #+1
 
-# initialize event time for censoring subjects
-# (must be greated than censoring time)
-Yinits = pbc$time + 5
-is.na(Yinits) <- pbc$status ==1
+tinits1<-pbc$time+500
+is.na(tinits1)<-pbc$status==1
+#tinits2<-tinits1+5
 
-#split data into train and test
 set.seed(8102)
 train_int <- sample(nrow(pbc),floor(nrow(pbc)*0.8))
 
-is_censored_train = is_censored[train_int]
-is_censored_test = is_censored[-train_int]
-Y_train = Y[train_int]
-Y_test = Y[-train_int]
-Y_cen_train = Y_cen[train_int]
-Y_cen_test = Y_cen[-train_int]
-Yinits_train = Yinits[train_int]
-#Yinits_test = Yinits[-train_int]
+is.censored.train = is.censored[train_int]
+is.censored.test  = is.censored[-train_int]
+t.train = t[train_int]; 
+t.test = t[-train_int]
+t.cen.train = t.cen[train_int]; 
+t.cen.test = t.cen[-train_int]
+tinits1.train = tinits1[train_int]; 
+tinits1.test = tinits1[-train_int]
+x.train = x[train_int,]; 
+x.test = x[-train_int,]
 
-X_train = X[train_int, ]
-X_test  = X[-train_int,]
-
-
-
-# --- JAGS_AFT function: Spike & Slab prior --- #
-JAGS_SpikeSlab = function(Y_train,X_train,X_test,Y_cen_train,Y_cen_test, n.iter=10000,n.burnin=1000){
-        
-        JAGS_AFT = function() {
-                # Likelihood
-                for (i in 1:n_train) {
-                        is_censored_train[i] ~ dinterval(Y_train[i], Y_cen_train[i])
-                        Y_train[i] ~ dexp(lambda_train[i])
-                        lambda_train[i] = exp(beta0 + inprod(X_train[i,],beta))
-                }
-                
-                #prior for beta
-                for(l in 1:p){
-                        beta[l] ~ dnorm(0,inv_tau2[l])
-                        inv_tau2[l] <- (1-gamma[l])*1000+gamma[l]*0.01
-                        gamma[l] ~ dbern(0.5)
-                }
-                
-                #prior for beta0
-                beta0 ~ dnorm(0, 0.0001)
-                
-                #prediction
-                for (i in 1:n_pred) {
-                        is_censored_test[i] ~ dinterval(Y_pred[i], Y_cen_test[i])
-                        Y_pred[i] ~ dexp(lambda_pred[i])
-                        lambda_pred[i] <- exp(beta0 + inprod(X_test[i,],beta))
-                }
+surv_model = function(){
+        for(i in 1:n_train) {
+                is.censored.train[i] ~ dinterval(t.train[i], t.cen.train[i])
+                t.train[i] ~ dexp(lambda.train[i])
+                lambda.train[i] = exp(beta0 + inprod(x.train[i,],beta))
         }
+        beta0 ~ dnorm(0,0.001)
         
+        for (i in 1:17) {beta[i]  ~ dnorm(0,0.001)}
         
-        AFT.data = list(Y_train=Y_train,
-                        is_censored_train=is_censored_train,
-                        is_censored_test =is_censored_test,
-                        Y_cen_train=Y_cen_train,
-                        Y_cen_test=Y_cen_test,
-                        X_train=X_train,
-                        X_test =X_test ,
-                        n_train=as.integer(nrow(X_train)),
-                        n_pred =as.integer(nrow(X_test)),
-                        p=ncol(X_train)
-        )
-        
-        
-        #set parameters to simulate
-        fit_JAGS_AFT = jags(
-                data  = AFT.data,
-                inits = list(list(
-                                beta=rnorm(17),
-                                beta0=rnorm(1),
-                                gamma=rep(1,length=17),
-                                Y_train=Yinits_train
-                )),
-                parameters.to.save = c("Y_pred", "beta0", "beta"),
-                n.chains=1,
-                n.iter=10000,
-                n.burnin=1000,
-                model.file=JAGS_AFT
-        )
-        
-        mcmc_fit = as.mcmc(fit_JAGS_AFT)
-        #predicted Y
-        Y_pred_sample = mcmc_fit[[1]][,paste("Y_pred[",1:nrow(X_test),"]",sep="")]
-        Y_pred=apply(Y_pred_sample,2,mean)
-        Y_pred_CI = apply(Y_pred_sample,2,quantile,prob=c(0.025,0.975))
-        #beta
-        beta_res = mcmc_fit[[1]][,c(paste0("beta[",1:17,"]"), "beta0")]
-        return(list(Y_pred=Y_pred,
-                    beta_res=beta_res,
-                    Y_pred_lcl = Y_pred_CI[1,],
-                    Y_pred_ucl = Y_pred_CI[2,],
-                    DIC = fit_JAGS_AFT$BUGSoutput$DIC,
-                    pD  = fit_JAGS_AFT$BUGSoutput$pD,#check model complexity(if model has extra parameter, pD is small)
-                    fit.JAGS=fit_JAGS_AFT))
+        for (i in 1:n_test) {
+                is.censored.test[i] ~ dinterval(t.pred[i], t.cen.test[i])
+                t.pred[i] ~ dexp(lambda.pred[i])
+                lambda.pred[i] = exp(beta0 + inprod(x.test[i,],beta))
+        }
 }
 
-plot_res = function(res,main=""){
-        plot(X_test$age,res$Y_pred_ucl,type="l",
-             ylim=c(0,5000),xlab="age/day",ylab="log(Time)",
-             cex.lab=1.5,cex.axis=1.5,main=main)
-        lines(X_test$age,res$Y_pred_lcl)
-        lines(X_test$age,res$Y_pred,col="blue")
-        points(X_test$age, Y_test,col="red")
-}
+pbcdata<-list(n_train=as.integer(length(t.train)), 
+                n_test =as.integer(length(t.test )), 
+                t.train=t.train,
+                is.censored.train=is.censored.train, is.censored.test=is.censored.test,
+                t.cen.train=t.cen.train, t.cen.test=t.cen.test,
+                x.train=x.train, x.test=x.test)
 
-summary_res = function(res,Y_test){
-        PMSE = mean((res$Y_pred-Y_test)^2)
-        coverage = mean((Y_test>res$Y_pred_lcl)&(Y_test<res$Y_pred_ucl))
-        return(c(PMSE=PMSE,coverage=coverage))
-}
+pbcinits<-list(list(t.train=tinits1.train,
+                      beta0=rnorm(1),
+                      beta =rnorm(17)))
 
-res_aft = JAGS_SpikeSlab(Y_train=Y_train,
-                         X_train=X_train,
-                         X_test=X_test,
-                         Y_cen_train,
-                         Y_cen_test)
+pbcjags = jags(
+        data = pbcdata,
+        inits = pbcinits,
+        parameters.to.save = c("t.pred","beta0", "beta"),
+        n.chains = 1,
+        n.iter = 50000,
+        n.burnin = 1000,
+        model.file = surv_model)
 
+print(pbcjags)
+
+pbc_mcmc = as.mcmc(pbcjags)
+par(ask=T)
+traceplot(pbc_mcmc)
+summary(pbc_mcmc)
+densplot(pbc_mcmc)
+
+View(pbc_mcmc[[1]])
